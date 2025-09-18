5cec4133b59cabdd10a3bb0fe40753ab6514eb73
AnkeetSah <ankeetkumar7777@gmail.com>
2025-09-18 23:34:08 +0530

    corrected url in the middleware1

--- middleware/checkNotLoggedIn.js
+++ middleware/checkNotLoggedIn.js
@@ -7,7 +7,7 @@ export const checkNotLoggedIn = (req, res, next) => {
   if (token) {
     try {
       jwt.verify(token, process.env.JWT_SECRET);
-
+      console.log(process.env.FRONTEND_URL)
       // Use FRONTEND_URL env variable
       const DASHBOARD_URL = (process.env.FRONTEND_URL || "http://localhost:5173") + "/user/dashboard";
 
