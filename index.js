366e6a7695a7fb692b7d4338abe491d59a86a900
AnkeetSah <ankeetkumar7777@gmail.com>
2025-09-18 22:44:01 +0530

    Updated the index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
 
-
-const PORT = 5000;
+// ✅ Use Render’s provided PORT or fallback to 5000 for local dev
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
